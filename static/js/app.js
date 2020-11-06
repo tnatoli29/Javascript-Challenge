731de0f52b823693fe86d321091f04e83c44c5bb
Thomas <tpnats@gmail.com>
2020-11-06 10:54:37 -0500

    5th commit, final product, added remove table function, removed console logs, and made the filter settings show on the website

--- static/js/app.js
+++ static/js/app.js
@@ -4,20 +4,34 @@ var tableData = data;
 
 //Using D3 we wil create a table to the website and populate it with the data
 //Grab the tbody from the HTML
-var tbody = d3.select("tbody");
+function displayTable(sightings){
 
+    var tbody = d3.select("tbody");
 
-tableData.forEach((UFO) => {
-    var row = tbody.append("tr");
+    sightings.forEach((UFO) => {
+
+     var row = tbody.append("tr");
+
 //set up row variable to add append a table row
-    Object.entries(UFO).forEach(([key, value]) => {
-// set up the keys and values for each entry and iterate over them
-    var cell = row.append("td");
-// append the data to a cell
-    cell.text(value);
-    });
-});
+        Object.entries(UFO).forEach(([key, value]) => {
 
+// set up the keys and values for each entry and iterate over them
+        var cell = row.append("td");
+
+// append the data to a cell
+        cell.text(value);
+        });
+    });
+};
+
+displayTable(tableData)
+
+// clear the table for new data
+function removeTbody() {
+  d3.select("tbody")
+    .selectAll("tr").remove()
+    .selectAll("td").remove();
+};
 
 
 var button = d3.select("#filter-btn");
@@ -29,20 +43,25 @@ form.on("submit",runEnter);
 function runEnter() {
 //Prevent the page from refreshing
     d3.event.preventDefault();
+    removeTbody();
+
 //Select the input element and get HTML node
     var inputElement = d3.select("#datetime");
+
 //Get the value property of input element
     var inputValue = inputElement.property("value");
-//Console log the user's input
-    console.log(inputValue);
-//Filter the data based on user's input
-    var filteredData = tableData.filter(date => date.datetime === inputValue);
-//Console log the filtered data based on the user's input
-    console.log(filteredData);
+
+    if (inputValue.trim() === "" ) {
+    // display the whole database if the date field has no date
+    var filteredData = tableData;
+
+  } else {
+    // otherwise, display the filtered dataset
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue.trim());
+  };
+
+    displayTable(filteredData)
 };
 
-
-//#############################
-//Part 2
 
 
